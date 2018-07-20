a4eaa78785f8ab58e8faf0810d7e30723c6ceefc
Zoe <zoereagan@gmail.com>
2018-07-19 19:05:24 -0500

    react strap on login page

--- src/App.js
+++ src/App.js
@@ -3,36 +3,46 @@ import logo from './logo.png';
 import './App.css';
 import UserFormContainer from './UserFormContainer';
 import LoginRegister from './LoginRegister';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      loggedIn: true
+      loggedIn: false
     }
   };
 
   render() {
     return (
       <div className="App-container">
+        <Grid>
+          <Row>
+            <Col sm={12} md={12}>
 
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Behest</h1>
-        </header>
+              <header className="App-header">
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                <h1 className="App-title">Behest</h1>
+              </header>
 
-        {this.state.loggedIn ?
-          <UserFormContainer />
+              {this.state.loggedIn ?
+                <UserFormContainer />
 
-          :
-          <LoginRegister />
-        }
+                :
+                <LoginRegister />
+              }
 
 
-        <footer>
-            <small>Based on the <a target="_blank" href="https://dementia-directive.org/">Dementia Directive</a>, created by Dr. Barak Gaster at the University of Washington.<br/>
-                   &copy; Zoe Skye, 2018</small>
-        </footer>
+              <footer>
+                  <small>Based on the <a target="_blank" href="https://dementia-directive.org/">Dementia Directive</a>, created by Dr. Barak Gaster at the University of Washington.<br/>
+                         &copy; Zoe Skye, 2018</small>
+              </footer>
+
+            </Col>
+          </Row>
+        </Grid>
+
+
         </div>
     );
   }
--- src/LoginRegister/index.js
+++ src/LoginRegister/index.js
@@ -1,4 +1,7 @@
 import React, {Component} from "react";
+import {Button, Form, FormGroup, Col, FormControl, ControlLabel, Grid, Row} from 'react-bootstrap';
+
+
 
 class LoginRegister extends Component{
   constructor(){
@@ -10,9 +13,50 @@ class LoginRegister extends Component{
 
   render(){
     return(
-      <div>
-        <h3>Login Here</h3>
-      </div>
+      <div className="login-form">
+        <Grid>
+          <Row className="show-grid">
+            <Col sm={12} md={12}>
+
+              <h4>If you are a new user, please register below</h4>
+              <Button className={this.state.registering ? "current" : null} className="button" >Create new user</Button><br /><br />
+
+              <h4>Returning user? Log in here.</h4>
+              <Button className={this.state.registering ? "current" : null} className="button" >Login</Button><br /><br />
+
+
+                <Form horizontal>
+                  <FormGroup controlId="formHorizontalEmail">
+                    <Col componentClass={ControlLabel} sm={2}>
+                      Email
+                    </Col>
+                    <Col xs={12} md={8}>
+                      <FormControl type="email" placeholder="Email" />
+                    </Col>
+                  </FormGroup>
+
+                  <FormGroup controlId="formHorizontalPassword">
+                    <Col componentClass={ControlLabel} sm={2}>
+                      Password
+                    </Col>
+                    <Col xs={12} md={8}>
+                      <FormControl type="password" placeholder="Password" />
+                    </Col>
+                  </FormGroup>
+
+                  <FormGroup>
+                    <Col xs={12} md={12}>
+                      <Button type="submit">Sign in</Button>
+                    </Col>
+                  </FormGroup>
+                </Form>
+
+            </Col>
+          </Row>
+        </Grid>
+
+    </div>
+
     )
   }
 }
